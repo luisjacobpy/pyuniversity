970907b13da1995f9c8f7c598e1b7983184b551f
Luis Hernandez <caphumleon@gmail.com>
2022-07-14 10:04:19 -0500

    Se agregan files de python

---
+++ Ejercicio detalle del libro.py
@@ -0,0 +1,8 @@
+#Ejerciocio N2
+print("Hola me gustaría saber de tu libro favorito")
+book_name = input("¿Cómo se llama el libro: ")
+writer = input("¿Cómo se llama el autor? ")
+print("Gracias por tu respuesta")
+print("El libro:", book_name, "fue escrito por:", writer)
+
+print("Fin del programa")
