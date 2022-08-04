de85a5af923be6010b56ed22c5e3f0d18372254b
Luis Jacobo <caphumleon@gmail.com>
2022-08-04 15:44:41 -0500

    Este es el avance del jueves

--- EjercicioNumeroParOImpar.py
+++ EjercicioNumeroParOImpar.py
@@ -4,3 +4,11 @@
 valor = int(input("Escribe un valor: "))
 valueMinimun = 0
 valorMaximo = 5
+
+#Los parentesís son opcionales
+dentroRango = (valor >= valueMinimun) and (valor <= valorMaximo)
+
+if dentroRango:
+    print(f"El valor {valor} está dentro de Rango.")
+else:
+    print(f"El valor {valor} está fuera de rango.")
